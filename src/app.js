5d44bd988f8df40bd3777a863605c6a8b540e9b0
Guillaume <iambluedev@gmx.fr>
2022-03-20 16:07:00 +0100

    feat: support aggregator

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const compression = require("compression");
 const path = require("path");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const moment = require("moment");
 
 const morgan = require("./middleware/morgan");
 
@@ -56,6 +57,39 @@ app.get("/api/chart", require("./http/charts").display);
 io.of("/sockets/datas").on("connection", () => {
   // Lorsqu'un client se connecte
   linky.log.debug("New client connected to sockets");
+});
+
+io.of("/sockets/aggregator").on("connection", (socket) => {
+  // Lorsqu'un aggreagator se connecte
+  linky.log.debug("New aggregator connected to sockets");
+
+  socket.on("data.array", async (data) => {
+    if (data.label === "PAPP") {
+      io.of("/sockets/datas").emit("data", data);
+    }
+
+    const { Mesure } = linky.schemas;
+
+    const mesure = await Mesure.findOne({
+      limit: 1,
+      order: [["at", "DESC"]],
+      where: {
+        label: data.label,
+        id_linky: data.linkyId,
+      },
+    });
+
+    if (!moment(mesure.at).isAfter(moment().subtract(5, "minutes"))) {
+      Mesure.create({
+        idLinky: data.linkyId,
+        value: data.value,
+        label: data.label,
+        at: moment(),
+      }).then(() => {
+        linky.log.debug(`New measures : ${data.label} - ${data.value}`);
+      });
+    }
+  });
 });
 
 module.exports = server;
